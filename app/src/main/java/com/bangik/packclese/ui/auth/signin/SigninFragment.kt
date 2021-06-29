76b64dfbb8ef872dd385ff3e8af1a17b4bf5fce2
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2021-06-29 17:05:58 +0700

    sign up address, success, toolbar & navigation fragment

--- app/src/main/java/com/bangik/packclese/ui/auth/signin/SigninFragment.kt
+++ app/src/main/java/com/bangik/packclese/ui/auth/signin/SigninFragment.kt
@@ -1,11 +1,15 @@
 package com.bangik.packclese.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bangik.packclese.MainActivity
 import com.bangik.packclese.R
+import com.bangik.packclese.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_signin.*
 
 class SigninFragment : Fragment() {
 
@@ -15,5 +19,21 @@ class SigninFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signin, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnSignup.setOnClickListener{
+            val signup = Intent(activity, AuthActivity::class.java)
+            signup.putExtra("page_request", 2)
+            startActivity(signup)
+        }
+
+        btnSignin.setOnClickListener{
+            val home = Intent(activity, MainActivity::class.java)
+            startActivity(home)
+            activity?.finish()
+        }
     }
 }
