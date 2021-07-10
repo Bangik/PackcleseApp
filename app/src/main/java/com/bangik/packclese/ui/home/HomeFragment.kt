a71197e693bc10d9ca44a0c72873c64845a5475e
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2021-07-10 09:34:15 +0700

    ui fragment home

--- app/src/main/java/com/bangik/packclese/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangik/packclese/ui/home/HomeFragment.kt
@@ -9,8 +9,7 @@ import com.bangik.packclese.R
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
